ed7b4ba6b5944c5e4d3a191dab0b08c4e70845ab
Tim Huang <timothy.huang@berkeley.edu>
2019-04-12 14:59:24 -0700

    marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,18 +5,21 @@ import "EBookToken.sol";
 
 contract Marketplace {
 
-  mapping(EBookToken => mapping(uint => Listing)) public listings;
+  mapping(EBookToken => Listing[]) public listings;
 
   struct Listing {
-    EBookToken _tokenContract;
+    EBookToken tokenContract;
     uint256 bookPrice;
     uint256 numCopies;
+    address payable owner;
   }
 
   function sell(EBookToken _tokenContract, uint256 _bookPrice, uint256 _numCopies) {
     require(_tokenContract.balanceOf(msg.sender) >= _numCopies);
-    
 
+    Listing storage l = Listing(_tokenContract, _bookPrice, _numCopies, msg.sender);
+
+    listings[_tokenContract].push(l);
 
 
   }
